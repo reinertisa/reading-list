6d9e3ab9fa8a1db8f4237a20e97b22cb3bb4a7f7
Isa Reinert <reinert.isa@gmail.com>
2025-01-10 23:42:32 -0800

    feat(website): create Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+
+
+export default function Navbar() {
+
+    return (
+        <nav>
+            <h1>Context app</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
